a92a290b760b1b76d714c24791969ff5be27a9cf
Benjamin Oakes <ben@benjaminoakes.com>
2011-11-28 22:30:35 -0500

    Basic untaring and gunziping

--- lib/airplane_mode/ruby_doc.rb
+++ lib/airplane_mode/ruby_doc.rb
@@ -5,11 +5,11 @@ require 'rubygems'
 require 'archive/tar/minitar'
 
 class AirplaneMode::RubyDoc
-  # # TODO
-  # def html
-  #   file = open('http://ruby-doc.org/downloads/ruby_1_9_2_core_rdocs.tgz')
-  #   tgz = Zlib::GzipReader.new(file)
-  #   # Warning: tgz will be closed!
-  #   Archive::Tar::Minitar.unpack(tgz, 'x')
-  # end
+  # FIXME (this is really barebones at the moment)
+  def html
+    file = open('http://ruby-doc.org/downloads/ruby_1_9_2_core_rdocs.tgz')
+    tgz = Zlib::GzipReader.new(file)
+    # Warning: tgz will be closed!
+    Archive::Tar::Minitar.unpack(tgz, 'output')
+  end
 end
