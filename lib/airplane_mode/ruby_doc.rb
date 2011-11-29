e38579e609b793e898c2304c1f0804ee01f7e800
Benjamin Oakes <ben@benjaminoakes.com>
2011-11-28 22:23:49 -0500

    (m) gzip notes

--- lib/airplane_mode/ruby_doc.rb
+++ lib/airplane_mode/ruby_doc.rb
@@ -7,7 +7,9 @@ require 'archive/tar/minitar'
 class AirplaneMode::RubyDoc
   # # TODO
   # def html
-  #   # file = open('http://ruby-doc.org/downloads/ruby_1_9_2_core_rdocs.tgz')
-  #   # Archive::Tar::Minitar.unpack(...)
+  #   file = open('http://ruby-doc.org/downloads/ruby_1_9_2_core_rdocs.tgz')
+  #   tgz = Zlib::GzipReader.new(file)
+  #   # Warning: tgz will be closed!
+  #   Archive::Tar::Minitar.unpack(tgz, 'x')
   # end
 end
